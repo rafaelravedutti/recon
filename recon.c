64d5fe70e62f945dbc901eecbd7ae26fe96d9d6c
Rafael Ravedutti Lucio Machado <rrlm13@inf.ufpr.br>
2016-08-22 19:01:30 -0300

    Write first version
    Signed-off-by: Rafael Ravedutti Lucio Machado <rrlm13@inf.ufpr.br>

--- recon.c
+++ recon.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 char *strfind(const char *string, char character) {
   char *ptr;
@@ -21,10 +26,32 @@ void range_scan(const char *range, unsigned int *first, unsigned int *last) {
   }
 }
 
+void scan_port(const char *address, unsigned int port) {
+  int sock;
+  struct sockaddr_in addr;
+
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  if(sock < 0) {
+    perror("socket");
+    return;
+  }
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = inet_addr(address);  
+  memset(addr.sin_zero, 0, sizeof(addr.sin_zero));
+
+  if(connect(sock, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0) {
+    perror("connect");
+    return;
+  }
+}
+
 int main(int argc, const char *argv[]) {
-  char range[32];
+  char range[32], address[32];
   unsigned int addr[3];
-  unsigned int first_addr, last_addr, first_port, last_port, i, j;
+  unsigned int first_addr, last_addr, first_port, last_port, count, total, i, j;
 
   if(argc != 3) {
     fprintf(stdout, "Uso: %s <address range> <port range>\n", argv[0]);
@@ -35,11 +62,16 @@ int main(int argc, const char *argv[]) {
   range_scan(range, &first_addr, &last_addr);
   range_scan(argv[2], &first_port, &last_port);
 
+  total = (last_addr - first_addr + 1) * (last_port - first_port + 1);
   for(i = first_addr; i <= last_addr; ++i) {
     for(j = first_port; j <= last_port; ++j) {
-      fprintf(stdout, "Scanning %u.%u.%u.%u:%u\n", addr[0], addr[1], addr[2], i, j);
+      snprintf(address, sizeof address, "%u.%u.%u.%u", addr[0], addr[1], addr[2], i);
+      fprintf(stdout, "\rScanning %s:%u (%.2f%%)", address, j, ((double) count / (double) total) * 100);
+      scan_port(address, j);
+      ++count;
     }
   }
 
+  fprintf(stdout, "\rScanning complete!                           \n");
   return 0;
 }

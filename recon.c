fa629fa4df2732778e983259e1d283995bc5a065
Rafael Ravedutti <rafaelraveduttilm@hotmail.com>
2016-08-27 18:39:03 -0300

    Write scan table
    Signed-off-by: Rafael Ravedutti <rafaelraveduttilm@hotmail.com>

--- recon.c
+++ recon.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+
+struct scan_table {
+  char address[32];
+  char banner[32];
+  unsigned short int port;
+  struct scan_table *next;
+};
 
 char *strfind(const char *string, char character) {
   char *ptr;
@@ -26,9 +34,10 @@ void range_scan(const char *range, unsigned int *first, unsigned int *last) {
   }
 }
 
-void scan_port(const char *address, unsigned int port) {
+void scan_port(const char *address, unsigned int port, struct scan_table **table) {
   int sock;
   struct sockaddr_in addr;
+  struct scan_table *entry;
 
   sock = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -43,12 +52,29 @@ void scan_port(const char *address, unsigned int port) {
   memset(addr.sin_zero, 0, sizeof(addr.sin_zero));
 
   if(connect(sock, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0) {
-    perror("connect");
     return;
   }
+
+  entry = (struct scan_table *) malloc(sizeof(struct scan_table));
+
+  if(entry != NULL) {
+    entry->port = port;
+    entry->next = *table;
+    entry->banner[0] = '\0';
+    strncpy(entry->address, address, sizeof entry->address);
+
+    if(*table != NULL) {
+      (*table)->next = entry;
+    } else {
+      *table = entry;
+    }
+  }
+
+  close(sock);
 }
 
 int main(int argc, const char *argv[]) {
+  struct scan_table *table, *entry;
   char range[32], address[32];
   unsigned int addr[3];
   unsigned int first_addr, last_addr, first_port, last_port, count, total, i, j;
@@ -67,11 +93,22 @@ int main(int argc, const char *argv[]) {
     for(j = first_port; j <= last_port; ++j) {
       snprintf(address, sizeof address, "%u.%u.%u.%u", addr[0], addr[1], addr[2], i);
       fprintf(stdout, "\rScanning %s:%u (%.2f%%)", address, j, ((double) count / (double) total) * 100);
-      scan_port(address, j);
+      scan_port(address, j, &table);
       ++count;
     }
   }
 
   fprintf(stdout, "\rScanning complete!                           \n");
+
+  for(entry = table; entry != NULL; entry = entry->next) {
+    fprintf(stdout, "%s\t%u", entry->address, entry->port);
+
+    if(entry->banner[0] != '\0') {
+      fprintf(stdout, "\t%s", entry->banner);
+    }
+
+    fprintf(stdout, "\n");
+  }
+
   return 0;
 }

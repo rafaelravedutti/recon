9f224bfd20ef2d9571337dcf14a11f40c493c0ad
Rafael Ravedutti Lucio Machado <rrlm13@inf.ufpr.br>
2016-08-29 08:39:23 -0300

    Add raw socket method and scan all ports when range not specified
    Signed-off-by: Rafael Ravedutti Lucio Machado <rrlm13@inf.ufpr.br>

--- recon.c
+++ recon.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
 
+/* Buffer length */
+#define BUFFER_LENGTH     256
+
+/* Request string */
+#define MAGIC_STRING      "Give me your banner now!\n"
+
+/* Scan table */
 struct scan_table {
   char address[32];
   char banner[32];
-  unsigned short int port;
+  unsigned int port;
   struct scan_table *next;
+};
+
+/* Pseudo header for checksum calculation */
+struct pseudo_header {
+  unsigned int source_address;
+  unsigned int dest_address;
+  unsigned char placeholder;
+  unsigned char protocol;
+  unsigned short tcp_length;
 };
 
 char *strfind(const char *string, char character) {
@@ -25,6 +44,18 @@ char *strfind(const char *string, char character) {
   return NULL;
 }
 
+unsigned short csum(unsigned short *data, unsigned int length) {
+  unsigned int i, sum = 0;
+
+  for(i = 0; i < length; ++i, ++data) {
+    sum += *data;
+  }
+
+  sum = (sum >> 16) + (sum & 0xFFFF);
+  sum += (sum >> 16);
+  return (unsigned short)(~sum);
+}
+
 void range_scan(const char *range, unsigned int *first, unsigned int *last) {
   if(strfind(range, '-') != NULL) {
     sscanf(range, "%u-%u", first, last);
@@ -34,25 +65,101 @@ void range_scan(const char *range, unsigned int *first, unsigned int *last) {
   }
 }
 
-void scan_port(const char *address, unsigned int port, struct scan_table **table) {
-  int sock;
+void scan_port(const char *address, unsigned int port, unsigned char use_raw_socket, struct scan_table **table) {
+  char buffer[BUFFER_LENGTH];
+  char pseudo_buffer[BUFFER_LENGTH];
+  int sock, one = 1;
   struct sockaddr_in addr;
   struct scan_table *entry;
-
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-
-  if(sock < 0) {
-    perror("socket");
-    return;
-  }
+  struct iphdr *ip;
+  struct tcphdr *tcp;
+  struct pseudo_header *phdr;
 
   addr.sin_family = AF_INET;
   addr.sin_port = htons(port);
   addr.sin_addr.s_addr = inet_addr(address);  
   memset(addr.sin_zero, 0, sizeof(addr.sin_zero));
 
-  if(connect(sock, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0) {
-    return;
+  if(use_raw_socket == 1) {
+    sock = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+
+    if(sock < 0) {
+      perror("socket");
+      return;
+    }
+
+    if(setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &one, sizeof(int)) < 0) {
+      perror("setsockopt");
+      close(sock);
+      return;
+    }
+
+    ip = (struct iphdr *) buffer;
+    ip->ihl = 5;
+    ip->version = 4;
+    ip->tos = 0;
+    ip->tot_len = sizeof(struct iphdr) + sizeof(struct tcphdr);
+    ip->id = htonl (54321); 
+    ip->frag_off = 0;
+    ip->ttl = 255;
+    ip->protocol = IPPROTO_TCP;
+    ip->check = 0;
+    ip->saddr = inet_addr(INADDR_ANY);
+    ip->daddr = addr.sin_addr.s_addr;
+       
+    tcp = (struct tcphdr *)(buffer + sizeof(struct iphdr));
+    tcp->source = htons(1234);
+    tcp->dest = htons(port);
+    tcp->seq = 0;
+    tcp->ack_seq = 0;
+    tcp->doff = 5;
+    tcp->fin = 0;
+    tcp->syn = 1;
+    tcp->rst = 0;
+    tcp->psh = 0;
+    tcp->ack = 0;
+    tcp->urg = 0;
+    tcp->window = htons(5840);
+    tcp->check = 0;
+    tcp->urg_ptr = 0;
+
+    phdr = (struct pseudo_header *) pseudo_buffer;
+    phdr->source_address = ip->saddr;
+    phdr->dest_address = ip->daddr;
+    phdr->placeholder = 0;
+    phdr->protocol = IPPROTO_TCP;
+    phdr->tcp_length = htons(sizeof(struct tcphdr));
+
+    memcpy(pseudo_buffer + sizeof(struct pseudo_header), tcp, sizeof(struct tcphdr));
+    tcp->check = csum((unsigned short *) pseudo_buffer, sizeof(struct pseudo_header) + sizeof(struct tcphdr));
+
+    if(sendto(sock, buffer, ip->tot_len, 0, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0) {
+      perror("sendto");
+      close(sock);
+      return;
+    }
+
+    if(recv(sock, buffer, sizeof buffer, 0) < 0) {
+      close(sock);
+      return;
+    }
+
+    if(tcp->rst == 1 || (tcp->syn == 0 && tcp->ack == 0)) {
+      close(sock);
+      return;
+    }
+  } else {
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(sock < 0) {
+      perror("socket");
+      return;
+    }
+
+    if(connect(sock, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0) {
+      close(sock);
+      return;
+    }
   }
 
   entry = (struct scan_table *) malloc(sizeof(struct scan_table));
@@ -78,29 +185,40 @@ int main(int argc, const char *argv[]) {
   char range[32], address[32];
   unsigned int addr[3];
   unsigned int first_addr, last_addr, first_port, last_port, count, total, i, j;
+  time_t now;
 
   table = NULL;
+  count = 0;
+  time(&now);
+
+  if(argc != 2 && argc != 3) {
+    fprintf(stdout, "Uso: %s <address range> [port range]\n", argv[0]);
+    return 0;
+  }
+
+  fprintf(stdout, "Varredura iniciada em %s", ctime(&now));
+  fprintf(stdout, "IP: %s\n", argv[1]);
+  fprintf(stdout, "Portas: %s\n", (argc != 3) ? ("*") : argv[2]);
+  fprintf(stdout, "---\n");
 
   if(argc != 3) {
-    fprintf(stdout, "Uso: %s <address range> <port range>\n", argv[0]);
-    return 0;
+    first_port = 0;
+    last_port = 65535;
+  } else {
+    range_scan(argv[2], &first_port, &last_port);
   }
 
   sscanf(argv[1], "%u.%u.%u.%s", &addr[0], &addr[1], &addr[2], range);
   range_scan(range, &first_addr, &last_addr);
-  range_scan(argv[2], &first_port, &last_port);
 
   total = (last_addr - first_addr + 1) * (last_port - first_port + 1);
   for(i = first_addr; i <= last_addr; ++i) {
     for(j = first_port; j <= last_port; ++j) {
       snprintf(address, sizeof address, "%u.%u.%u.%u", addr[0], addr[1], addr[2], i);
-      fprintf(stdout, "\rScanning %s:%u (%.2f%%)", address, j, ((double) count / (double) total) * 100);
-      scan_port(address, j, &table);
+      scan_port(address, j, 0, &table);
       ++count;
     }
   }
-
-  fprintf(stdout, "\rScanning complete!                           \n");
 
   for(entry = table; entry != NULL; entry = entry->next) {
     fprintf(stdout, "%s\t%u", entry->address, entry->port);
